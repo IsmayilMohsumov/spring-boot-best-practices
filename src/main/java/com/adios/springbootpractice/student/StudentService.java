4b054adef38ed4899b834f5af0bf51f23a8377f2
IsmayilMohsumov <mohsumovismayil@gmail.com>
2022-07-15 11:58:04 +0200

    Adding business logic

--- src/main/java/com/adios/springbootpractice/student/StudentService.java
+++ src/main/java/com/adios/springbootpractice/student/StudentService.java
@@ -3,7 +3,9 @@ package com.adios.springbootpractice.student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +16,17 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()){
+            throw new IllegalStateException(String.format("Email [%s] is taken",student.getEmail()));
+        }
         studentRepository.save(student);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, Student student) {
+        Student studentFoundById = studentRepository.findById(studentId).get();
+        studentFoundById.setName(student.getName());
+        studentFoundById.setEmail(student.getEmail());
     }
 }
